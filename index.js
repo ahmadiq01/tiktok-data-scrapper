19a9142acc7f681eb0153ef8d61631fb0255974f
Ahmad Qureshi <89626382+ahmadiq01@users.noreply.github.com>
2025-01-08 10:20:33 +0500

    Issue Fixed For URL

--- index.js
+++ index.js
@@ -29,31 +29,44 @@ app.post("/scrape", async (req, res) => {
     // Extract data
     const data = await page.evaluate(() => {
       const videoUrl = document.querySelector("video source")?.src || null;
-      const thumbnail = document
-        .querySelector("div.css-8czf5p-StyledVideoBlurBackground img")
-        ?.src || null;
-      const title = document
-        .querySelector("h1[data-e2e='browse-video-desc'] span")
-        ?.innerText.trim() || null;
-      const duration = document
-        .querySelector("div.css-1cuqcrm-DivSeekBarTimeContainer")
-        ?.innerText.trim() || null;
+      const thumbnail =
+        document.querySelector("div.css-8czf5p-StyledVideoBlurBackground img")
+          ?.src || null;
+      const title =
+        document
+          .querySelector("h1[data-e2e='browse-video-desc'] span")
+          ?.innerText.trim() || null;
+      const duration =
+        document
+          .querySelector("div.css-1cuqcrm-DivSeekBarTimeContainer")
+          ?.innerText.trim() || null;
 
       return { videoUrl, thumbnail, title, duration };
     });
 
     // Close the browser
     await browser.close();
+    console.log(data);
+    console.log(data.videoUrl);
+    console.log(data.thumbnail);
+    console.log(data.title);
+    console.log(data.duration);
 
+    console.log(
+      !data.videoUrl || !data.thumbnail || !data.title || !data.duration
+    );
     // Check if all required fields were extracted
     if (!data.videoUrl || !data.thumbnail || !data.title || !data.duration) {
+      console.log("going to if")
       return res
         .status(500)
-        .json({ error: "Failed to extract video data. Structure might have changed." });
+        .json({
+          error: "Failed to extract video data. Structure might have changed.",
+        });
     }
-    data.videoUrl=url;
+    data.videoUrl = url;
     // Respond with the extracted data
-    // res.json(data);
+    res.json(data);
   } catch (error) {
     console.error("Error scraping TikTok:", error.message);
     res.status(500).json({ error: "Failed to scrape the TikTok page." });
